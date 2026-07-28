5daf48da874ab7d319fe5c391c87706f59163a36
Gregory L. <72673408+aleph-beth@users.noreply.github.com>
2026-07-28 08:43:26 +0200

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'static',
   server: {
     host: '0.0.0.0',
     port: 3000,
   },
 });
+
